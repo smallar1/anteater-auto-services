42b505c2c14fec16b4b4f72ce36637ed173b87f5
Tai Tran <ttran052899@gmail.com>
2025-05-08 03:13:05 -0700

    - Added Logo, Background Images to Navbar & Services

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import './css/Home.css';
+import './Home.css';
 
 function Home() {
   return (
-    <div className="home-page"> {/* Wrapper with background image */}
-      <div className="home">
-        <div className="mission-statement">
-          <h2>Our Mission</h2>
-          <p>Temporary Mission Statement</p>
-          <p>Blah blah blah</p>
-        </div>
+    <div className="home">
+      <header>
+      <div className="mission-statement">
+        <h2>Our Mission</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, architecto ex veniam itaque tempora quo laborum optio, porro nihil, sunt nulla corporis est blanditiis repellendus enim nostrum ipsa quod quibusdam.
+        </p>
       </div>
+      <div className="scheduling-column">
+        <h2>Schedule Today!</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos aspernatur, voluptatum animi ex aut non. Provident reprehenderit, illum consequatur voluptatibus cum distinctio facere modi autem debitis commodi officia voluptatem ex!</p>
+      </div>
+      </header>
+      <body>
+
+      </body>
     </div>
   );
 }
 
-export default Home;
+export default Home; 
