e4307bc3557eff736bbd14c7b0057ba9a80461df
Samuel Mallari <smallar1@uci.edu>
2025-05-31 20:42:20 -0700

    testing github pages

---
+++ src/components/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SignUp } from '@clerk/clerk-react';
+
+function SignUpPage() {
+  return (
+    <div className="signup-container">
+      <SignUp />
+    </div>
+  );
+}
+
+export default SignUpPage; 
